8beee6fc9dea70b73f444da0fb3154bedaaef253
Masahito-H <masahito.hayashi.8p@gmail.com>
2021-07-28 16:22:54 +0900

    via atom

--- components/WorksCanvas.js
+++ components/WorksCanvas.js
@@ -12,7 +12,7 @@ let EffectComposer = null, RenderPass = null, ShaderPass = null;
 export default class WorksCanvas extends Component{
     constructor(props){
         super(props);
-
+        
         this.canvasStyle = {
             position: "absolute",
             width: "100vw",
@@ -21,14 +21,14 @@ export default class WorksCanvas extends Component{
             left: "0"
         };
     }
-
+    
     componentDidMount(){
         EffectComposer = require("three/examples/jsm/postprocessing/EffectComposer").EffectComposer;
         RenderPass = require("three/examples/jsm/postprocessing/RenderPass").RenderPass;
         ShaderPass = require("three/examples/jsm/postprocessing/ShaderPass").ShaderPass;
         extend({ EffectComposer, RenderPass, ShaderPass });
     }
-
+    
     render(){
         return(
             <Canvas style={this.canvasStyle} camera={{near: -200, far: 2000, position: [0, 0, 1000]}} orthographic={true} onCreated={
@@ -44,7 +44,7 @@ export default class WorksCanvas extends Component{
 const ImageScene = (props) => {
     const { size } = useThree(),
     pCam = useRef();
-
+    
     return (
         <Fragment>
             <perspectiveCamera ref={pCam} aspect={size.width / size.height} fov={60} near={.1} far={2000} position={[0, 0, 2000]} />
@@ -57,7 +57,7 @@ const ImageScene = (props) => {
                     }
                     else{
                         let pfId = parseInt(Router.router.asPath.slice(7, 10));
-
+                    
                         return (
                             <Fragment>
                                 <WorkImage pfId={pfId} camera={pCam.current} delta={props.delta} setDelta={props.setDelta} workScrollSwitch={props.workScrollSwitch} setWorkScrollSwitch={props.setWorkScrollSwitch} />
@@ -72,13 +72,13 @@ const ImageScene = (props) => {
 
 const ThumbImage = (props) => {
     const loader = new THREE.TextureLoader();
-
+    
     const thumbImgRef = useRef(),
     cl = new THREE.Clock(),
     { size } = useThree();
-
+    
     const blank = new THREE.Texture(null);
-
+    
     useEffect(() => {
         if(props.indexNum > -1){
             thumbImgRef.current.material.map = loader.load(pfData[props.indexNum].thumbnail.imgsrc, () => {
@@ -92,18 +92,18 @@ const ThumbImage = (props) => {
             thumbImgRef.current.material.map = blank;
         }
     }, [props.indexNum]);
-
+    
     useEffect(() => {
-
+        
     }, []);
-
+    
     return (
         <mesh ref={thumbImgRef} position={[0, 0, 0]} scale={[1, 1, 1]}>
             <planeGeometry attach="geometry" args={[size.width, size.height]} />
             <meshBasicMaterial attach="material" map={blank} />
         </mesh>
     );
-
+    
 };
 
 const WorkImage = (props) => {
@@ -112,7 +112,7 @@ const WorkImage = (props) => {
     groupRef2 = useRef(),
     cl = new THREE.Clock(),
     { size } = useThree();
-
+    
     const loader = new THREE.TextureLoader(),
     srcArray = pfData[props.pfId].thumbnail.detailsrcs;
 
@@ -120,29 +120,29 @@ const WorkImage = (props) => {
     srcArray.forEach((data, i) => {
         textureArray.push(loader.load("../" + data, () => {
             let img = groupRef2.current.children[i].material.map.image;
-            //console.log(yPos);
             if(img){
                 let aspectProd = (img.height / img.width) * (size.width / size.height);
                 groupRef2.current.children[i].scale.set(.75, 1.25 * aspectProd, 1);
                 groupRef2.current.children[i].position.y = yPos;
                 yPos -= (size.height / 4 + size.height / 8) * aspectProd;
-                //console.log(yPos);
             }
         }));
     });
     const [texArray, setTexArray] = useState(textureArray);
 
     const [velocity, setVelocity] = useState(0);
-
+    
     useEffect(() => {
         if(props.workScrollSwitch){
             setVelocity(props.delta * .1);
             props.setWorkScrollSwitch(false);
         }
     }, [props.workScrollSwitch]);
-
+    
     useFrame(({ gl }) => void ((gl.autoClear = false), (groupRef.current.visible = true), gl.render(workImgRef.current, props.camera), (groupRef.current.visible = false)));
     useFrame(() => {
+        let groupRef2LastChild = groupRef2.current.children[groupRef2.current.children.length - 1];
+        
         if(velocity !== 0 && (velocity < 1 && velocity > -1)){
             setVelocity(0);
         }
@@ -150,12 +150,10 @@ const WorkImage = (props) => {
             groupRef2.current.position.y = 0;
             setVelocity(velocity * .8);
         }
-        /*
-        else if(groupRef2.current.position.y > size.height / 2 * (groupRef2.current.children.length - 1)){
-            groupRef2.current.position.y = size.height / 2 * (groupRef2.current.children.length - 1);
+        else if(groupRef2.current.position.y > -groupRef2LastChild.position.y){
+            groupRef2.current.position.y = -groupRef2LastChild.position.y;
             setVelocity(velocity * .8);
         }
-        */
         else if(velocity > 0){
             groupRef2.current.translateY(velocity);
             setVelocity(velocity - 1);
@@ -165,8 +163,8 @@ const WorkImage = (props) => {
             setVelocity(velocity + 1);
         }
     });
-
-
+    
+    
     return (
         <scene ref={workImgRef}>
             <group ref={groupRef} position={[0, 0, 1000]} scale={[1, 1, 1]} rotation={[0, Math.PI / 4, 0]} visible={false}>
@@ -181,7 +179,7 @@ const WorkImage = (props) => {
                                     </mesh>
                                 );
                             });
-
+                            
                             return images;
                         })()
                     }
@@ -195,10 +193,10 @@ const WorkImage = (props) => {
 const Effect = (props) => {
     const { gl, scene, camera, size } = useThree();
     const effectRef = useRef();
-
+    
     useEffect(() => void effectRef.current.setSize(size.width, size.height), [size]);
     useFrame(() => effectRef.current.render(), 1);
-
+    
     return (
         <effectComposer ref={effectRef} args={[gl]}>
             <renderPass attachArray="passes" args={[scene, camera]} clear={false} />
